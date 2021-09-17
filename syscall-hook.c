ffb61bbcedb41a4d5ca0099bbec9bfb9cbfb6d9c
2ck <yannick.loeck@gmail.com>
2021-09-18 00:02:52 +0200

    Find address of sys_call_table

--- syscall-hook.c
+++ syscall-hook.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kprobes.h>
 
 #ifdef pr_fmt
 #undef pr_fmt
@@ -9,11 +10,41 @@
 MODULE_LICENSE("GPL");
 
 
-int init_module(void) {
+static struct kprobe kp = {
+    .symbol_name = "kallsyms_lookup_name"
+};
+
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+
+unsigned long sys_call_table;
+
+
+int init_module(void)
+{
+    kallsyms_lookup_name_t kallsyms_lookup_name;
+
     pr_info("module loaded\n");
+
+    /*
+     * From kernel 5.7.0 onwards, kallsyms_lookup_name
+     * is no longer exported by default. This workaround
+     * uses kprobes to find the address of the function.
+    */
+    register_kprobe(&kp);
+    kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+    unregister_kprobe(&kp);
+
+    sys_call_table = kallsyms_lookup_name("sys_call_table");
+    if (!sys_call_table) {
+        pr_debug("cannot find the sys_call_table address\n");
+        return -1;
+    } else {
+        pr_info("found sys_call_table at %lx\n", sys_call_table);
+    }
     return 0;
 }
 
-void cleanup_module(void) {
+void cleanup_module(void)
+{
     pr_info("module unloaded\n");
 }
