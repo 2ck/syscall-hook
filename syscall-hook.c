7e1226f197236c9ec5d3d00090690406dcb81da4
2ck <yannick.loeck@gmail.com>
2021-09-18 17:28:08 +0200

    Add write protection toggle

--- syscall-hook.c
+++ syscall-hook.c
@@ -7,6 +7,9 @@
 #endif
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+/* bit 16 (for the CR0 register) */
+#define WP_MASK 0x10000
+
 MODULE_LICENSE("GPL");
 
 
@@ -17,6 +20,42 @@ static struct kprobe kp = {
 typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
 
 unsigned long sys_call_table;
+
+/*
+ * From kernel 5.0 onwards, the write protect bit of
+ * cr0 cannot be changed using the normal write_cr0 function.
+ * This custom function uses asm to directly access the register.
+ * The "memory" clobber prevents reordering of the asm statement.
+ */
+static inline void custom_write_cr0(unsigned long val)
+{
+    asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
+}
+
+void write_protect_wrapper(void (*wrapped_worker)(void))
+{
+    unsigned long cr0;
+
+    /* disable write protection */
+    cr0 = read_cr0();
+    custom_write_cr0(cr0 & ~WP_MASK);
+
+    /* execute callback */
+    wrapped_worker();
+
+    /* re-enable write protection */
+    custom_write_cr0(cr0);
+}
+
+void create_hook(void)
+{
+
+}
+
+void remove_hook(void)
+{
+
+}
 
 
 int init_module(void)
@@ -41,10 +80,17 @@ int init_module(void)
     } else {
         pr_info("found sys_call_table at %lx\n", sys_call_table);
     }
+
+    /* hook and replace the syscall */
+    write_protect_wrapper(&create_hook);
+
     return 0;
 }
 
 void cleanup_module(void)
 {
+    /* restore the original syscall */
+    write_protect_wrapper(&remove_hook);
+
     pr_info("module unloaded\n");
 }
